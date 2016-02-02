3dfd28e6b731d27eace1aeb2082738f73ce61562
Vadzim Vysotski <Vadzim_Vysotski@epbyminw4159.minsk.epam.com>
2016-02-02 16:09:50 +0300

    initial commit

---
+++ src/XamarinBetterVectorDrawableApp/DroidApplication.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Android.App;
+using Com.Bettervectordrawable;
+using Android.Runtime;
+
+namespace XamarinBetterVectorDrawableApp
+{
+	[Application]
+	public class DroidApplication : Application
+	{
+		public DroidApplication(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+		{
+		}
+
+		public override void OnCreate ()
+		{
+			VectorDrawableCompat.EnableResourceInterceptionFor (Resources, Resource.Drawable.ic_pets_black);
+			base.OnCreate();
+		}
+	}
+}
+
---
+++ src/XamarinBetterVectorDrawableApp/MainActivity.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace XamarinBetterVectorDrawableApp
+{
+	[Activity (Label = "XamarinBetterVectorDrawable", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+			SetContentView (Resource.Layout.Main);
+		}
+	}
+}
+
+
